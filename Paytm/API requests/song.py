11efe808e085815350f84ac2dbbf427ba66dafe9
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2021-05-13 09:51:52 +0530

    Update song.py

--- Paytm/API requests/song.py
+++ Paytm/API requests/song.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.post('https://iot-dev.paytm.in/download/unbind.wav?name=paytm', headers={"Auth-Token":"f23992229db875e41fa7e28c55da1651f864ac7f495d913a5af5aabd659c72a1"})
+response = requests.post('URI', headers={"Auth-Token":"token"})
 
 with open('output.wav', 'wb') as f:
 	f.write(response.content)

e2834220dc974e5bc1b0343547bdc781ca8711bb
Sundaram Dubey <96574271+sundaram-dubey@users.noreply.github.com>
2022-01-16 21:05:32 +0530

    Update rest.c

--- rest.c
+++ rest.c
@@ -1,10 +1,11 @@
-#include <cpr/cpr.h>
 #include<stdio.h>
 #include<stdlib.h>
-int main(int argc, char** argv) {
+#include <cpr/cpr.h>
 
-	
-    cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/users/maze-runnar"});
+
+
+int main(int argc, char** argv) {
+    cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/users/maze-runnar"});  
     printf("%d\n", r.status_code);
 
     printf("%s\n", r.text);
